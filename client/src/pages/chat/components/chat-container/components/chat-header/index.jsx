09838644bf7d80a17817e8c1f0473f913484cdc9
Gurinder Singh <gurinder.singh@psquarecompany.com>
2026-05-21 14:56:23 +0530

    minor fix

--- client/src/pages/chat/components/chat-container/components/chat-header/index.jsx
+++ client/src/pages/chat/components/chat-container/components/chat-header/index.jsx
@@ -17,7 +17,7 @@ function ChatHeader() {
                     <Avatar className="h-12 w-12 rounded-full overflow-hidden">
                       {selectedChatData.image ? (
                         <AvatarImage
-                          src={`${HOST}/${selectedChatData.image}`}
+                          src={`${selectedChatData.image}`}
                           alt="profile"
                           className="object-cover w-full h-full bg-black rounded-full"
                         />
